703484850781d8b398818a50bb13289d834b9e45
nadyaOleynikova <kposhkanadya@yandex.ru>
2022-08-19 22:50:49 +0700

    errors

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,15 +1,12 @@
 import java.util.Random;
 
 public class MagicBox<T> {
-    protected int size; //размер
+    int occupied = 0; // занято
     protected T[] items;
 
     public MagicBox(int size) {
-        this.size = size;
         items = (T[]) new Object[size];
     }
-
-    int occupied = 0; // занято
 
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
@@ -23,18 +20,16 @@ public class MagicBox<T> {
     }
 
     public T pick() {
-        for (int i = 0; i < items.length; i++) {
-            if (occupied < items.length) {
-                throw new RuntimeException("Cвободных ячеек: " + (items.length - occupied));
-            }
-            if (occupied == items.length) {
-                Random random = new Random();
-                int randomInt = random.nextInt(items.length);
-                System.out.println("Коробка полна.");
-                System.out.println("Случайный элемент: " + items[randomInt]);
-                System.out.println("~~~~~~~~~~~~~~~~~");
-                return items[randomInt];
-            }
+        if (occupied < items.length) {
+            throw new RuntimeException("Cвободных ячеек: " + (items.length - occupied));
+        }
+        if (occupied == items.length) {
+            Random random = new Random();
+            int randomInt = random.nextInt(items.length);
+            System.out.println("Коробка полна.");
+            System.out.println("Случайный элемент: " + items[randomInt]);
+            System.out.println("~~~~~~~~~~~~~~~~~");
+            return items[randomInt];
         }
         return null;
     }
